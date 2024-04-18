f8f75e066c87858ff7f3e4dd74754ec7322235c2
Omar Shalabi <omarshalabi856@gmail.com>
2024-04-18 23:24:00 +0300

    Added first test view

---
+++ Controllers/ClubController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SocialSphere___MVC.Controllers
+{
+    public class ClubController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
