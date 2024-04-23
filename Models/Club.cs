a07810c44bac173f7db08dc2256c37e9369459c4
Omar Shalabi <omarshalabi856@gmail.com>
2024-04-23 23:09:46 +0300

    Added input validation for club create

--- Models/Club.cs
+++ Models/Club.cs
@@ -8,16 +8,23 @@ namespace SocialSphere___MVC.Models
     {
         [Key]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
         public string? Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
         public string? Description { get; set; }
+
         public string? Image { get; set; }
+
         [ForeignKey("Address")]
         public int? AddressId { get; set; }
         public Address? Address { get; set; }
+
         public ClubCategory ClubCategory { get; set; }
+
         [ForeignKey("AppUser")]
         public string? AppUserId { get; set; }
         public AppUser? AppUser { get; set; }
     }
-
 }
